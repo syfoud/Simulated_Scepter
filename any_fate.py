9443f2364e89654673a1cc0df2353f36d34bcbc9
歪比巴卜 <ozfire1234@163.com>
2026-09-09 00:05:46 +0800

    Update any_fate.py

--- any_fate.py
+++ any_fate.py
@@ -212,8 +212,7 @@ class AnyFateUniverse(SimulatedUniverse):
                     CUS_LOGGER.info("梦中那刺骨的愤怒与对自我的憎恨仍在震动着他的心。")
                     # 根据用户设置决定是否遇猪切换2号位角色
                     if self.opt.get("pig_switch_2_role", False):
-                        key_mouse_manager.press("2")
-                        self.current_role = 2
+                        self.current_role(num=2)
                         self.quan = 0
                         self.bai_e = 0
                 else:
