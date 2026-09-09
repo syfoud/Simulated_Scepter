46c152915f90600d4b864d8eecd6e30414e364e5
歪比巴卜 <ozfire1234@163.com>
2026-09-09 18:59:26 +0800

    iron_blood.py: “放弃骰子效果”增加视觉佐证，调用方法以使用

--- iron_blood.py
+++ iron_blood.py
@@ -178,9 +178,7 @@ class IronBloodUniverse(AnyFateUniverse):
                 self.click_text(text="确认目标", box=[1635, 1735, 968, 996])
             else:
                 CUS_LOGGER.info("所以你才变成了这副模样：残缺的神像…悲哀的薪柴。")
-                if self.click_text(text="放弃", box=[1221, 1276, 967, 998]):
-                    key_mouse_manager.wait()
-                    key_mouse_manager.click(1147, 676) # 点击“确认”
+                self.abandon_confirm(confirm=True)
         elif "战争" in text:
             try:
                 self.try_analysis_map(mode=2)
@@ -206,14 +204,10 @@ class IronBloodUniverse(AnyFateUniverse):
             else:
                 #战争崇拜无猪可改，放弃
                 CUS_LOGGER.info("「放心，我会替你照顾。」")
-                if self.click_text(text="放弃", box=[1221, 1276, 967, 998]):
-                    key_mouse_manager.wait()
-                    key_mouse_manager.click(1147, 676) # 点击“确认”
+                self.abandon_confirm(confirm=True)
         elif "毁灭" in text:
             #其它节点一律放弃
-            if self.click_text(text="放弃", box=[1221, 1276, 967, 998]):
-                key_mouse_manager.wait()
-                key_mouse_manager.click(1147, 676) # 点击“确认”
+            self.abandon_confirm(confirm=True)
 
     def select_go(self):
         num = extract_number(match_numbers_in_region(self.screen))
