957f1af2028fdf858bd443fbfa217255743500b9
歪比巴卜 <ozfire1234@163.com>
2026-09-11 14:51:04 +0800

    完善“进入第三面暂停”功能

--- iron_blood.py
+++ iron_blood.py
@@ -33,6 +33,7 @@ class IronBloodUniverse(AnyFateUniverse):
         self.first_plane_count = self.opt.get("first_plane", 14)
         self.second_plane_count = self.opt.get("second_plane", 31)
         self.first_plane_min_weight = self.opt.get("first_plane_min_weight", 6)
+        self.third_plane_pause_enabled = self.opt.get("third_plane_pause_enabled", False)
         self.third_plane_pause_count = self.opt.get("third_plane_pause_count", 0)
         self.fate = "毁灭"
         self.my_fate = config.fates.index(self.fate)
@@ -138,8 +139,8 @@ class IronBloodUniverse(AnyFateUniverse):
         elif self.click_text(text="消褪",box=[10, 220, 0, 112],click=False,warning=False):
             self.plane_floor=3
             # 达到第三面暂停需求数时停止程序
-            if self.third_plane_pause_count > 0 and self.third_plane_pause_count <= (self.kill_count+1):
-                CUS_LOGGER.info(f"当前击杀数：{self.kill_count+1}，已达到第三面暂停需求数：{self.third_plane_pause_count}，停止程序")
+            if self.third_plane_pause_enabled and self.third_plane_pause_count <= (self.kill_count + 1):
+                CUS_LOGGER.debug(f"当前击杀数：{self.kill_count + 1}，已达到第三面暂停需求数：{self.third_plane_pause_count}，停止程序")
                 self.stop()
                 return
         else:
